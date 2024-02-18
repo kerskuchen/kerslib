dc920a49101803668d51f4ea5c8b1bdd8ec25bb5
kerskuchen <j.schwab@mail.de>
2024-02-18 14:29:14 +0100

    add gitignore

--- setup_project_in_parent_folder.bat
+++ setup_project_in_parent_folder.bat
@@ -1,7 +1,11 @@
 @echo off
 
+REM NOTE: make sure our current working directory is the same as our batchfile directory
+cd /D "%~dp0"
+
 robocopy .\project\ ..\ "launch_vscode.bat"
 robocopy .\project\ ..\ ".clang-format"
+robocopy .\project\ ..\ ".gitignore"
 robocopy .\project\.vscode ..\.vscode /s /e
 echo DO NOT EDIT > ..\.vscode\GENERATED_FILES_DO_NOT_EDIT
 
